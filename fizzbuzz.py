e0733c8b6af264ff3bbd43420fd2648c89082036
Asianguy123 <sas@drsyed.org>
2021-06-15 16:00:23 +0100

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+# FizzBuzz
+stop = 100
+for i in range(1, stop + 1):
+    fb_nums = [3, 5, 7, 8, 12]
+    fb_outs = ['Fizz', 'Buzz', 'Whizz', 'Bang', 'Snap']
+    output = ''
+    for j in range(len(fb_nums)):
+        if not(i % fb_nums[j]):
+            output += fb_outs[j]
+    if len(output) == 0:
+        output += str(i)
+    print(output)
