8df4f849b9865e3a78187ad687d5843ba2227a9f
Asianguy123 <sas@drsyed.org>
2021-06-18 19:22:41 +0100

    Add files via upload

--- kruskals_algorithm.py
+++ kruskals_algorithm.py
@@ -1,5 +1,11 @@
 # Kruskal's Minimum Spanning Tree Algorithm
 
+# sort edges in ascending weight order #DONE
+# pick lowest weight edge #DONE
+# pick next edge - add if it doesnt create a cycle 
+# print MST and weight
+
+
 def test_one():
 # expected answer = 20
     edges = [['AB', 7],
@@ -44,3 +50,38 @@ def test_three():
             ['78', 7]
     ]
     return edges
+#, test_two(), test_three()
+tests = [test_one()]
+for edges in tests:
+    nodes = set()
+    for edge in edges:
+        nodes.add(edge[0][0])
+        nodes.add(edge[0][1])
+    tree_nodes = set()
+    edges.sort(key = lambda x: x[1])
+    print(edges)
+    for edge in edges:
+        print(tree_nodes)
+        if len(tree_nodes) == 0:
+            tree_nodes.add(edge[0][0])
+            tree_nodes.add(edge[0][1])
+        elif edge[0][0] in tree_nodes:
+            if edge[0][1] in tree_nodes:
+                edges.remove(edge)
+            else:
+                tree_nodes.add(edge[0][1])
+        elif edge[0][1] in tree_nodes:
+            if edge[0][0] in tree_nodes:
+                edges.remove(edge)
+        else:
+            tree_nodes.add(edge[0][0])
+            tree_nodes.add(edge[0][1])
+    mst_weight = 0
+    for edge in edges:
+        connection = f'{edge[0][0]} ---- {edge[0][1]} : Weight = {edge[1]}'
+        mst_weight += edge[1]
+        print(connection)
+    print(edges)
+    print(mst_weight)
+
+# currently broken, will return to it at a later date
