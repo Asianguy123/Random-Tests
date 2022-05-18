74d293e2a78ccef8fbb290b5bdaf7e846f455e34
Asianguy123 <sas@drsyed.org>
2022-05-18 14:13:55 +0100

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -2,3 +2,13 @@
 
 # Factorial calculation
 # Tower of Hanoi logic problem
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Factorial Calculator
+
+def factorial_calc(num):
+    out = 1
+    if num != 0:
+        for i in range(2, num + 1):
+            out *= i
+    return num
