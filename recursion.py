ddb71ec4fb51369f674f54fee6e6f0efda35e78e
Asianguy123 <sas@drsyed.org>
2022-05-19 12:02:22 +0100

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -26,7 +26,11 @@ def recursive_factorial_calc(x):
         return 1
     else:
         return x * recursive_factorial_calc(x - 1)
-
+   
 def run_factorial_calc():
+    '''
+    Input and calls factorial calc function (currently recursive one)
+    '''
+
     num = int(input('Enter the integer you would like to find the factorial of:  '))
-    print(recursive_factorial_calc(num))    
+    print(recursive_factorial_calc(num))
