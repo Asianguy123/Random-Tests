16ba6d515f335e04eeec0693b76623d93862d27d
Asianguy123 <sas@drsyed.org>
2022-09-16 10:10:58 +0100

    Create Kruskals_MST.py

---
+++ Kruskals_MST.py
@@ -0,0 +1,10 @@
+# Kruskal's Minimum Spanning Tree Algorithm
+
+'''
+- sort edges in ascending weight order
+- pick lowest weight edge
+- pick next edge - add if it doesnt create a cycle 
+- print MST and weight
+'''
+
+# network contruction format: [[weight, node_one, node_two]] ==> all integers with (0 indexing for nodes)
