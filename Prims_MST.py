29cb286c5415d33f00d28c552f1f85ce599c85ef
Asianguy123 <sas@drsyed.org>
2022-09-24 01:26:13 +0100

    Update Prims_MST.py

--- Prims_MST.py
+++ Prims_MST.py
@@ -1,1 +1,10 @@
 # Prim's Minimum Spanning Tree Algorithm
+
+'''
+Choose any start vertex
+Select an edge of least weight that joins a new vertex to the tree
+    => if equal edges, pick any
+- Repeat until all vertices added
+'''
+
+# network contruction format: [[weight, node_one, node_two], []...]
