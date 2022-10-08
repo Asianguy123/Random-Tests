f46af678341f160517718aae57731438194ecf73
Asianguy123 <sas@drsyed.org>
2022-10-08 17:16:59 +0100

    Update triangulation.py

--- triangulation.py
+++ triangulation.py
@@ -4,3 +4,13 @@
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Check Functions
+
+def valid_input(sides):
+    for side in sides:
+        try:
+            val = int(side)
+            if val <= 0:
+                return False
+        except ValueError:
+            return False
+    return True
