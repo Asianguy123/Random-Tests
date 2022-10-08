a864007ffa10e446f6f172c28e0d3212f36d4f31
Asianguy123 <sas@drsyed.org>
2022-10-08 17:16:45 +0100

    Update triangulation.py

--- triangulation.py
+++ triangulation.py
@@ -1,1 +1,6 @@
-# Triangulation
+# Triangulation 
+
+# determine if a triangle is formed, and what kind, given side lengths
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Check Functions
