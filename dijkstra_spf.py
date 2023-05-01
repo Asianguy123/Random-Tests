4b0af878295bc011f38a1a5d10327a887a2338ab
Asianguy123 <sas@drsyed.org>
2023-05-01 05:11:54 +0100

    Update dijkstra_spf.py

--- dijkstra_spf.py
+++ dijkstra_spf.py
@@ -4,3 +4,11 @@ class Graph:
      
     def __init__(self, graph):
         self.graph = graph
+
+    def display_shortest_path(self, visited, source, destination):
+        self.current_node = destination
+        self.path = destination
+        while self.current_node != source:
+            self.previous_node = visited[self.current_node][1]
+            self.path = self.previous_node + self.path  
+            self.current_node = visited[self.current_node][1]
