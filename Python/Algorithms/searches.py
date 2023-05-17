0bfd8764914c8f41348333c84da911eff9c6a173
Asianguy123 <sas@drsyed.org>
2023-05-17 03:00:22 +0100

    Add files via upload

---
+++ Python/Algorithms/searches.py
@@ -0,0 +1,44 @@
+# Linear and Binary searches
+
+def linear_search(array, element):
+    '''
+    Linear search:
+    - can be performed on unsorted lists
+    - using a flag (for demonstrative purposes)
+    - time = O(n)
+    '''
+
+    i = 0
+    flag = False
+    while not flag:
+        if i == len(array):
+            return 'Element not in array'
+        if array[i] == element:
+            return i
+        i += 1
+
+def binary_search(array, element):
+    '''
+    Binary search:
+    - performed only on sorted data (this function accepts unsorted too)
+    - 
+    '''
+
+    array = sorted(array)
+    flag = False
+    index = 0
+    while not flag:
+        i = len(array) // 2
+        if array[i] == element:
+            index += i
+            flag = True
+        if i == 0:
+            return 'Element not in array'
+        if array[i] < element:
+            index += i
+            array = array[i:]
+        elif array[i] > element:
+            array = array[:i] 
+    return index
+
+print(binary_search([1, 2, 6, 3, 4, 10, 9, 5, 3, 5, 13], 15))
