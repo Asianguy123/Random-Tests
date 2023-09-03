b0b7c6f89228c5ea631228126be3fdc81d96807f
Asianguy123 <sas@drsyed.org>
2023-09-03 23:47:18 +0100

    Update forward_kinematics.py

--- Python/forward_kinematics.py
+++ Python/forward_kinematics.py
@@ -1,1 +1,12 @@
 # Forward Kinematics
+
+'''
+Using forward kinematics to create tentacle oscillation (sinusoidal)
+FK uses joint hierarchy e.g. upper leg > lower leg > foot > toes
+'''
+# ---------------------------------------------------------------------------------------------------------------------
+# Imports
+
+import sys
+import pygame
+import math
