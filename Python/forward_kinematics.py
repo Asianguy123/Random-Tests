f8251f57a687935c1992d75be180b14710bedf6c
Asianguy123 <sas@drsyed.org>
2023-09-03 23:47:48 +0100

    Update forward_kinematics.py

--- Python/forward_kinematics.py
+++ Python/forward_kinematics.py
@@ -10,3 +10,16 @@ FK uses joint hierarchy e.g. upper leg > lower leg > foot > toes
 import sys
 import pygame
 import math
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Segement Classes
+
+class SegmentParent():
+
+    '''
+    SEGMENT PARENT CLASS:
+
+    - creates a root segment from all other segments are joined from
+    - pivots around a fixed point
+    - calculations of segment end points done via polar-cartesian conversion
+    '''
